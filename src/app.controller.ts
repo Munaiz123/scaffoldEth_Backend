26229eee41da7f0e9c5f1af0de45bffa053cc4fc
MunaizAhmed <munaizahmed2@gmail.com>
2024-11-23 09:25:38 -0600

    Continuing Lesson 15 - Added POST method to controller with minTokensDTO (data transfer object) to typeCheck POST payload, checking for MINTER_ROLE. Next: Start of Lesson 16- integrating with front end

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MintTokenDto } from './dtos/mintTokens.dto';
+
 
 @Controller()
 export class AppController {
@@ -31,7 +33,26 @@ export class AppController {
   }
 
   @Get('transaction-receipt')
-  async getTransactionReceipt(@Query('hash') hash: string) {
+  async getTransactionReceipt(@Query('hash') hash: string
+  // @Query('lol') lol: number
+  )
+  {
     return {result: await this.appService.getTransactionReceipt(hash)};
+  }
+
+
+  @Get('server-wallet-address')
+  async getServerWalletAddress() {
+    return {result: await this.appService.getServerWalletAddress()};
+  }
+
+  @Get('check-minter-role')
+  async checkMinterRole(@Query('address') address: string) {
+    return {result: await this.appService.checkMinterRole(address)};
+  }
+
+  @Post('mint-tokens')
+  async mintTokens(@Body() body: MintTokenDto) {
+    return {result: await this.appService.mintTokens(body.address)};
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import * as tokenJson from "./assets/MyToken.json"
-import { createPublicClient, http, Address, formatEther} from 'viem';
+import { createPublicClient, http, Address, formatEther, createWalletClient} from 'viem';
 import {sepolia} from 'viem/chains'
 import { ConfigService } from '@nestjs/config';
+import { privateKeyToAccount } from 'viem/accounts';
 
 
 @Injectable()
 export class AppService {
-  private publicClient; 
+ 
+  
+  publicClient; 
+  walletClient; 
 
   constructor(private configService: ConfigService) {
-
+    const account = privateKeyToAccount(`0x${this.configService.get<string>('MUNZY_TEST_PRIVATE_KEY')}`);
+    
     this.publicClient = createPublicClient({
       chain: sepolia,
       transport: http(this.configService.get<string>('RPC_ENDPOINT_URL')),
+    });
+
+
+    this.walletClient = createWalletClient({
+      transport: http(process.env.RPC_ENDPOINT_URL),
+      chain: sepolia,
+      account
     });
 
   }
@@ -26,6 +38,8 @@ export class AppService {
   getContractAddress(): Address {
     return this.configService.get<Address>('TOKEN_ADDRESS');
   }
+
+  async getServerWalletAddress() { return this.walletClient.account.address; }
 
   async getTokenName(): Promise<string> {
    
@@ -55,11 +69,57 @@ export class AppService {
   }
 
   async getTokenBalance(address: string): Promise<string>{
-    return null;
+
+    const symbol = await this.publicClient.readContract({
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: "symbol"
+    });
+
+    const balanceOf = await this.publicClient.readContract({
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: "balanceOf",
+      args:[address]
+    });
+
+    return `${formatEther(balanceOf as bigint)} ${symbol}`
   }
 
   async getTransactionReceipt(hash: string): Promise<string>{
-    return null;
+    let txn =  await this.publicClient.getTransactionReceipt({hash})
+    return `Transaction status: ${txn.status} || Block No: ${txn.blockNumber}`
   }
 
+  
+
+  async checkMinterRole(address: string): Promise<string> {
+    const MINTER_ROLE = await this.publicClient.readContract({
+
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: "MINTER_ROLE"
+    });
+
+    const hasRole = await this.publicClient.readContract({
+      address: this.getContractAddress(),
+      abi: tokenJson.abi,
+      functionName: "hasRole",
+      args:[MINTER_ROLE, address]
+    });
+
+    return `The address ${address} ${hasRole ? "has" : "does not have"} MINTER_ROLE for the smart contract.`
+
+  }
+
+
+  async mintTokens(address: string) {
+    // TODO:
+    //1. use the server wallet to create a contract instance attached to the smart contract address
+    //2. use the signed contract instance to send the mint txn
+    //3. wait for txn hash
+    //4. return txn hash
+
+    return {address}
+  }
 }
